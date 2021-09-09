bad22c619bbddf8c6bfd5d4e8d1e1c94a7a11a3a
Emma Paris <eparis@atlassian.com>
2021-09-09 11:54:26 -0300

    .

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../Home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+import Calculadora from "../LandingPage/Calculadora";
+
+
+
+
+function Home() {
+  return (
+    <React.Fragment>
+      
+      <Calculadora/>
+     
+    </React.Fragment>
+  );
+}
+
+
+export default Home;
